f25b91e5c8b2b47027b61a11eb782cd20c7a0aeb
EC2 Default User <ec2-user@ip-172-31-47-205.ap-northeast-1.compute.internal>
2022-12-08 13:43:08 +0000

    [Add]route controller

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,24 @@ Rails.application.routes.draw do
     registrations: "public/registrations",
     sessions: "public/sessions"
   }
+
+  namespace :admin do
+    resources :users, only:[:index, :show, :update]
+    resources :posts, only:[:index, :show, :destroy]
+    get '/search' => 'searches#search'
+  end
+
+  scope module: :public do
+    resource :users, only:[:show] do
+      collection do
+        patch "information/update" => "customers#update", as:"update"
+        get "information/edit" => "customers#edit", as:"edit"
+        get 'unsubscribe'
+        patch 'withdraw'
+      end
+    end
+    resources :posts, only:[:index, :show, :new, :create, :destroy]
+    get '/search' => 'searches#search'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
