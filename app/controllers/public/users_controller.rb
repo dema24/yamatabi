82c94b993d48a631de1c588e1e8523a61813f611
EC2 Default User <ec2-user@ip-172-31-47-205.ap-northeast-1.compute.internal>
2022-12-12 10:28:13 +0000

    退会機能の追加

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,7 +1,7 @@
 class Public::UsersController < ApplicationController
 
   def index
-    @users = User.page(params[:page])
+    @users = User.where(is_deleted: false).page(params[:page]).per(10)
   end
 
   def show
@@ -9,13 +9,11 @@ class Public::UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
-    if @user.update(user_params)
-      redirect_to user_path(@user), notice: "会員情報を更新しました"
+    if current_user.update(user_params)
+      redirect_to user_path(current_user), notice: "会員情報を更新しました"
     else
       render "edit"
     end
@@ -25,12 +23,15 @@ class Public::UsersController < ApplicationController
   end
 
   def withdraw
+    current_user.update(is_deleted: true)
+    reset_session
+    redirect_to root_path
   end
-  
+
   private
-  
+
   def user_params
     params.require(:user).permit(:name, :profile_image, :introduction, :email, :is_deleted)
   end
-  
+
 end
