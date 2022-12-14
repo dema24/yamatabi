27eb909c6b9e41eb0d23ecb3cdd0ec7c478ee737
EC2 Default User <ec2-user@ip-172-31-47-205.ap-northeast-1.compute.internal>
2022-12-14 09:29:24 +0000

    [Add]いいね、コメント機能（非同期通信）

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,16 @@
 class Post < ApplicationRecord
-  
+
   belongs_to :user
-  
+  has_many :post_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  has_many :post_tags, dependent: :destroy
+
+  validates :title, length:{maximum:20}
+  validates :body, length:{maximum:200}
+
   has_many_attached :images
+
+  def favorited_by?(user)
+    favorites.where(user_id: user.id).exists?
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
 
   #顧客用
-  devise_for :admin, skip: [:registrations, :passwords], controller: {
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
   }
-  
+
   namespace :admin do
-    resources :users, only:[:index, :show, :update]
+    resources :users, only:[:index, :show, :edit, :update, :destroy]
     resources :posts, only:[:index, :show, :destroy]
     get '/search' => 'searches#search'
   end
@@ -19,10 +19,16 @@ Rails.application.routes.draw do
 
   scope module: :public do
     resources :users, only:[:index, :show, :edit, :update] do
-        get 'unsubscribe'
-        patch 'withdraw'
+      get 'unsubscribe'
+      patch 'withdraw'
+      resource :relationships, only: [:create, :destroy]
+      get 'followings' => 'relationships#followings', as: 'followings'
+      get 'followers' => 'relationships#followers', as: 'followers'
     end
-    resources :posts, only:[:index, :show, :new, :create, :destroy]
+    resources :posts, only:[:index, :show, :new, :create] do
+      resources :post_comments, only: [:create, :destroy]
+      resource :favorites, only: [:create, :destroy]
+    end
     get '/search' => 'searches#search'
     root to: 'homes#top'
     get 'home/about' => 'homes#about', as: 'about'
