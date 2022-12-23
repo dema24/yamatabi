94754896115e6cd27a976c3172a1c0135a392f57
EC2 Default User <ec2-user@ip-172-31-47-205.ap-northeast-1.compute.internal>
2022-12-23 16:32:43 +0000

    [Update]View

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -22,7 +22,11 @@ class Public::SessionsController < Devise::SessionsController
   def guest_sign_in
     user = User.guest
     sign_in user
-    redirect_to root_path, notice: 'ゲストユーザーとしてログインしました。'
+    redirect_to user_path(user), notice: 'ゲストユーザーとしてログインしました。'
+  end
+
+  def after_sign_in_path_for(resource)
+    user_path(resource)
   end
 
   protected
